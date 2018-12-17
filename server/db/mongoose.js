613ae58bd7ff6e115cc62abc6048698bcd33706d
Sabeel Ahmad <sabeelbasharat@gmail.com>
2018-12-17 14:32:57 +0530

    GET /todos/:id route added

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,4 +4,6 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-module.exports.mongoose = mongoose;
+module.exports = {
+  mongoose,
+};
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -31,10 +32,33 @@ app.get('/todos', (req, res) => {
   // Fetch all todos from db
   Todo.find().then((todos) => {
     // Send all todos as response
-    res.send({todos}); 
+    res.send({todos});
   }, (e) => {
     // Error handler - 400 status - bad request
     res.status(400).send(e);
+  });
+});
+
+// GET /todos/:id - Get a particular todo
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  // Validating ID
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send(); // Sending an empty body and 404 status code
+  }
+
+  // Querying DB
+  Todo.findById(id).then((todo) => {
+    // Case - Valid ID, but not found in db
+    if(!todo) {
+      return res.status(404).send();
+    }
+    // Case - Todo found
+    res.status(200).send({todo});
+  }, (e) => {
+    // Error handler for querying db
+    res.status(404).send();
   });
 });
 
